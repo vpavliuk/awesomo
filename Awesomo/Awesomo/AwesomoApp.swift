1258f414f9dfb3d566d5183ac9cd01826b837271
Volodymyr Pavliuk <vpavl@softserveinc.com>
2021-07-09 23:11:35 +0300

    Empty Xcode project

---
+++ Awesomo/Awesomo/AwesomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomoApp.swift
+//  Awesomo
+//
+//  Created by Volodymyr Pavliuk on 09.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct AwesomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awesomo/Awesomo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Awesomo
+//
+//  Created by Volodymyr Pavliuk on 09.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
