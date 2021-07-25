11ba664ef3a54c6fe69389de403281309bdd082b
Volodymyr Pavliuk <vpavl@softserveinc.com>
2021-07-26 00:27:48 +0300

    Utils: Optimize TwoWayInterface

--- Awesomo/Utils/Sources/Utils/TwoWayInterface.swift
+++ Awesomo/Utils/Sources/Utils/TwoWayInterface.swift
@@ -13,30 +13,27 @@ public protocol TwoWayInterface {
    associatedtype InputSubject: Subject where InputSubject.Failure == Never
    var input: InputSubject { get }
 
-   associatedtype OutputPublisher: Publisher
+   associatedtype OutputPublisher: Publisher where OutputPublisher.Failure == Never
    var output: OutputPublisher { get }
 }
 
 public extension TwoWayInterface {
-   func eraseToAny() -> AnyTwoWayInterface<
-      InputSubject.Output,
-      OutputPublisher.Output,
-      OutputPublisher.Failure
-   > { AnyTwoWayInterface(self) }
+   func eraseToAny() -> AnyTwoWayInterface<InputSubject.Output, OutputPublisher.Output> {
+      AnyTwoWayInterface(self)
+   }
 }
 
 // Type-erased version of TwoWayInterface
-public struct AnyTwoWayInterface<Input, Output, OutputFailure: Error> {
+public struct AnyTwoWayInterface<Input, Output> {
 
    init<Base: TwoWayInterface>(_ base: Base) where
       Base.InputSubject.Output == Input,
-      Base.OutputPublisher.Output == Output,
-      Base.OutputPublisher.Failure == OutputFailure {
+      Base.OutputPublisher.Output == Output {
 
       input = AnySubscriber(base.input)
       output = base.output.eraseToAnyPublisher()
    }
 
    public let input: AnySubscriber<Input, Never>
-   public let output: AnyPublisher<Output, OutputFailure>
+   public let output: AnyPublisher<Output, Never>
 }
