23615abdc5100aa8e5340d6fff62a3c81fb71fab
Volodymyr Pavliuk <vpavl@softserveinc.com>
2021-07-25 19:55:46 +0300

    Rename IdType -> IDType

--- Awesomo/Utils/Sources/Utils/IDType.swift
+++ Awesomo/Utils/Sources/Utils/IDType.swift
@@ -1,12 +1,12 @@
 //
-//  IdType.swift
+//  IDType.swift
 //  Utils
 //
 //  Created by Volodymyr Pavliuk on 18.07.2021.
 //  Copyright © 2021 Volodymyr Pavliuk. All rights reserved.
 //
 
-public protocol IdType: Hashable {
+public protocol IDType: Hashable {
    associatedtype Value
    init(value: Value)
 }
