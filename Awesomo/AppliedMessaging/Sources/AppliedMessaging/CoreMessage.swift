4efac7767b37b4e45ba1bce1a56d05ece53f7c61
Volodymyr Pavliuk <vpavl@softserveinc.com>
2021-07-30 18:36:12 +0300

    Remove Applied Messaging

--- Awesomo/AppliedMessaging/Sources/AppliedMessaging/CoreMessage.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CoreMessage.swift
-//  AppliedMessaging
-//
-//  Created by Volodymyr Pavliuk on 11.07.2021.
-//  Copyright © 2021 Volodymyr Pavliuk. All rights reserved.
-//
-
-import Foundation
