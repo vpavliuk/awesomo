30fa49b548e37f99163e321a70078236a21b4e71
Volodymyr Pavliuk <vpavl@softserveinc.com>
2021-11-03 14:24:13 +0200

    expectLater extension on XCTestCase

---
+++ Awesomo/TestUtils/Sources/TestUtils/XCTestCaseExtensions.swift
@@ -0,0 +1,43 @@
+//
+//  XCTestCaseExtensions.swift
+//  TestUtils
+//
+//  Created by Volodymyr Pavliuk on 03.09.2021.
+//
+
+import XCTest
+import Combine
+
+extension XCTestCase {
+   public func expectLater<P: Publisher>(
+      _ publisher: P,
+      output: [P.Output],
+      timeout: TimeInterval = 1,
+      file: StaticString = #file,
+      line: UInt = #line
+   ) where P.Output: Equatable {
+
+      var actualOutput: [P.Output] = []
+      let expectation = expectation(description: "Awaiting publisher")
+
+      let cancellable = publisher.sink(
+         receiveCompletion: { _ in
+            expectation.fulfill()
+         },
+         receiveValue: { value in
+            actualOutput.append(value)
+         }
+      )
+
+      waitForExpectations(timeout: timeout)
+      cancellable.cancel()
+
+      if actualOutput != output {
+         XCTFail(
+            "Outputs don't match",
+            file: file,
+            line: line
+         )
+      }
+   }
+}
