aec8a8f600eec13491ab19503335fce8611b9318
Volodymyr Pavliuk <gwotan@gmail.com>
2022-05-07 20:39:03 +0300

    Add input for PeerDiscovery in app

--- Awesomo/MessagingApp/Sources/MessagingApp/MessagingApp.swift
+++ Awesomo/MessagingApp/Sources/MessagingApp/MessagingApp.swift
@@ -4,12 +4,19 @@ import Combine
 
 public struct MessagingApp<NetworkAddress, ContentNetworkRepresentation> {
    public init() {
+      peerDiscoveryInterfaceInternal = PublishingSubscriber()
+      peerDiscoveryInterface = AnySubscriber(peerDiscoveryInterfaceInternal)
+
       transportInterfaceInternal = PassthroughTwoWayInterface()
       transportInterface = transportInterfaceInternal.eraseToAny()
    }
 
-   public let transportInterface: AnyTwoWayInterface<TransportInterfaceInput, TransportInterfaceOutput>
-   private let transportInterfaceInternal: PassthroughTwoWayInterface<TransportInterfaceInput, TransportInterfaceOutput>
+   public typealias PeerDiscoveryInput = PeerAvailabilityEvent<NetworkAddress>
+   public let peerDiscoveryInterface: AnySubscriber<PeerDiscoveryInput, Never>
+   private let peerDiscoveryInterfaceInternal: PublishingSubscriber<PeerDiscoveryInput, Never>
+
    public typealias TransportInterfaceInput = InputFromTransport<NetworkAddress, ContentNetworkRepresentation>
    public typealias TransportInterfaceOutput = TransportSendRequest<NetworkAddress, ContentNetworkRepresentation>
+   public let transportInterface: AnyTwoWayInterface<TransportInterfaceInput, TransportInterfaceOutput>
+   private let transportInterfaceInternal: PassthroughTwoWayInterface<TransportInterfaceInput, TransportInterfaceOutput>
 }
