e00b214fdc39ae9c2045224b4d89211ed6910640
Volodymyr Pavliuk <gwotan@gmail.com>
2022-06-18 13:35:19 +0300

    Wire up peer discovery

--- Awesomo/MessagingApp/Sources/MessagingApp/MessagingApp.swift
+++ Awesomo/MessagingApp/Sources/MessagingApp/MessagingApp.swift
@@ -2,13 +2,30 @@ import Utils
 import Domain
 import Combine
 
-public struct MessagingApp<NetworkAddress, ContentNetworkRepresentation> {
+public final class MessagingApp<NetworkAddress: Equatable, ContentNetworkRepresentation> {
    public init() {
       peerDiscoveryInterfaceInternal = PublishingSubscriber()
       peerDiscoveryInterface = AnySubscriber(peerDiscoveryInterfaceInternal)
 
       transportInterfaceInternal = PassthroughTwoWayInterface()
       transportInterface = transportInterfaceInternal.eraseToAny()
+   }
+
+   public func wireUp() {
+      wireUpPeerDiscovery()
+   }
+
+   private func wireUpPeerDiscovery() {
+      let s = peerDiscoveryInterfaceInternal.publisher.sink { [weak self] availabilityEvent in
+         guard let self = self else { return }
+         let oldActivePeers = self.activePeers
+         self.updateActivePeers(event: availabilityEvent)
+         if self.activeScreen == .conversationList
+               && oldActivePeers != self.activePeers {
+
+         }
+      }
+      subscriptions.insert(s)
    }
 
    public typealias PeerDiscoveryInput = PeerAvailabilityEvent<NetworkAddress>
@@ -19,4 +36,23 @@ public struct MessagingApp<NetworkAddress, ContentNetworkRepresentation> {
    public typealias TransportInterfaceOutput = TransportSendRequest<NetworkAddress, ContentNetworkRepresentation>
    public let transportInterface: AnyTwoWayInterface<TransportInterfaceInput, TransportInterfaceOutput>
    private let transportInterfaceInternal: PassthroughTwoWayInterface<TransportInterfaceInput, TransportInterfaceOutput>
+
+   private typealias Peer = Domain.Peer<NetworkAddress>
+   private enum ActiveScreen: Equatable { case conversationList, selectedConversation(Peer.ID) }
+   private var activeScreen: ActiveScreen = .conversationList
+   private var activePeers: [Peer] = []
+
+
+
+   private var subscriptions = Set<AnyCancellable>()
+
+   private func updateActivePeers(event: PeerAvailabilityEvent<NetworkAddress>) {
+      let eventPeerIds = event.peers.map(\.id)
+      var updatedActivePeers = activePeers
+      updatedActivePeers.removeAll { eventPeerIds.contains($0.id) }
+      if event.change == .found {
+         updatedActivePeers.append(contentsOf: event.peers)
+      }
+      activePeers = updatedActivePeers
+   }
 }
